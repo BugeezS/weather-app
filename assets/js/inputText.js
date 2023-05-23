feed6ba3c5c03ff328d0655be5ea25634c184119
Bruno <brunolb.lafont@gmail.com>
2023-05-23 12:24:08 +0200

    custom search bar

---
+++ assets/js/inputText.js
@@ -0,0 +1,68 @@
+const cityInput = document.querySelector(".city_choice__input_text");
+const autocompleteList = document.querySelector(".autocomplete-list");
+const button = document.querySelector(".city_choice__button"); // Replace "button" with the selector for your button element
+
+// Function to hide the autocomplete list
+function hideAutocompleteList() {
+  autocompleteList.style.display = "none";
+}
+
+cityInput.addEventListener("input", () => {
+  const inputText = cityInput.value;
+
+  // Call the city API to fetch matching city names
+  fetchCityNames(inputText)
+    .then((data) => {
+      const cityNames = data.geonames.map((result) => result.name);
+      displayAutocompleteOptions(cityNames);
+    })
+    .catch((error) => {
+      console.error("Error fetching city names:", error);
+    });
+});
+
+// Event listener for the Enter key press
+cityInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    hideAutocompleteList();
+  }
+});
+
+// Event listener for the button click
+button.addEventListener("click", () => {
+  hideAutocompleteList();
+});
+
+export function fetchCityNames(query) {
+  const username = "bugeez"; // Replace with your GeoNames username
+  const url = `http://api.geonames.org/searchJSON?q=${query}&maxRows=5&username=${username}`;
+
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Unable to fetch city names.");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw new Error("Error fetching city names:", error);
+    });
+}
+
+export function displayAutocompleteOptions(cityNames) {
+  // Clear previous autocomplete options
+  autocompleteList.innerHTML = "";
+
+  cityNames.forEach((name) => {
+    const option = document.createElement("li");
+    option.textContent = name;
+    option.addEventListener("click", () => {
+      cityInput.value = name;
+      hideAutocompleteList();
+    });
+    autocompleteList.appendChild(option);
+  });
+
+  // Show autocomplete options
+  autocompleteList.style.display = "block";
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-import {getLocation} from "./getLocation.js";
+import { getLocation } from "./getLocation.js";
 import { background } from "./background.js";
 import { chart } from "./charts.js";
+import { fetchCityNames, displayAutocompleteOptions } from "./inputText.js";
 
 getLocation();
 background();
+fetchCityNames();
+displayAutocompleteOptions();
 chart();
